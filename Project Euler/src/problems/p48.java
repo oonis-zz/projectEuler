fadefd470c93d19d514858aee1a63e6ca37bf68e
Sam Macaluso <willsc8forwings@gmail.com>
2013-12-06 15:17:32 -0500

    finished 48!

--- Project Euler/src/problems/p48.java
+++ Project Euler/src/problems/p48.java
@@ -6,7 +6,7 @@
 
 package problems;
 
-import java.math.BigDecimal;
+import java.math.BigInteger;
 
 /**
  *
@@ -14,13 +14,20 @@ import java.math.BigDecimal;
  */
 public class p48 {
     public static void main(String[] args) {
-        BigDecimal result = BigDecimal.ZERO;
-        for( int i = 1 ; i <= 1000; i++ ){
-            result = result.add( BigDecimal.valueOf( Math.pow(i, i) ) );
+        new p48().runTest();
+        
+    }
+    
+    private void runTest(){
+        
+        
+        BigInteger result = BigInteger.ZERO;
+        BigInteger max = new BigInteger("1000");
+        for( BigInteger i = BigInteger.ONE; i.compareTo(max) <=0; i = i.add(BigInteger.ONE) ){
+            result = result.add( i.pow( i.intValue() ) );
         }
         
-        System.out.println("Solution:: " + result.toString());
-        
+        System.out.println("Solution:: " + result.toString()); // Just cut the last 10 from the output 
     }
     
 }
