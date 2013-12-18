31817810cab3fb471446bef2a526cbf917b3cccd
Sam Macaluso <Sam@Jiajie-PC>
2013-12-18 14:59:51 -0500

    work towards 54

--- Project Euler/src/problems/p54.java
+++ Project Euler/src/problems/p54.java
@@ -13,24 +13,29 @@ public class p54 {
     private static class Card{
         public final int rank;
         public final int suit;
-        //private final String ranks = "23456789TJQKA";
-        //private final char[] suites = "SHCD";
+        private static final String ranks = "23456789TJQKA";
+        private static final String suites = "SHCD";
         public Card( int rank, int suit ){
             this.rank = rank;
             this.suit = suit;
         }
         
         public Card( String s ){
-            this( "23456789TJQKA".indexOf( s.charAt(0) ), "SHCD".indexOf( s.charAt(1) ) );
+            this( ranks.indexOf( s.charAt(0) ), suites.indexOf( s.charAt(1) ) );
         }
         
-        // This function will return the hash value whch is rank times k + suit + c
-        public int hashValue(  ){
+        @Override
+        public int hashCode(  ){
             return ( rank * 4 + suit + 1 ); // k = 4, c = 1. Do we even need c?
         }
         
         @Override
         public boolean equals( Object obj ){
+            if( obj instanceof Card ) {
+            } else {
+                return false;
+            }
+            
             Card temp = (Card)obj;
             return ( rank == temp.rank && suit == temp.suit );
         }
