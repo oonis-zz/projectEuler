2271927490e584bc00e0ef739040154518438d60
Sam Macaluso <willsc8forwings@gmail.com>
2013-12-20 12:16:53 -0500

    Just adjusting the Driver

--- Project Euler/src/Driver/Driver.java
+++ Project Euler/src/Driver/Driver.java
@@ -1,6 +1,6 @@
 package Driver;
 import util.Problem;
-import problems.solved.p55;
+import problems.solved.*;
 
 /**
  *
@@ -8,7 +8,7 @@ import problems.solved.p55;
  */
 public class Driver {
     public static void main(String[] args) {
-        Problem p = new p55();
+        Problem p = new p57();
         System.out.println( "Problem " + p.getID() + " : " +
                 p.getTitle() );
         System.out.println( "Solution:: " + p.run() );
