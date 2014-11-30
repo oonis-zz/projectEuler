d51d6d6566abb6cf2ef28865abfcbe8aac1bf65f
Sam Macaluso <willsc8forwings@gmail.com>
2014-11-30 06:21:31 -0500

    Starting problem 207

---
+++ Project Euler/src/problems/unsolved/p207.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package problems.unsolved;
+
+import util.Problem;
+
+/**
+ * 
+ * @author Sam <willsc8forwings@gmail.com>
+ */
+public class p207 implements Problem{
+    
+    public static void main(String[] args) {
+        System.out.println( new p207().run() );
+    }
+    
+    @Override
+    public int getID() {
+        return 207;
+    }
+
+    @Override
+    public String getTitle() {
+        return "Integer partition equations";
+    }
+
+    @Override
+    public String run() {
+        
+        
+        
+        return "";
+    }
+
+}
